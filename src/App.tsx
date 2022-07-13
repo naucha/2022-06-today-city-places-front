1e7dacef942d79ffa967150d322567afe91c8f36
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-07-13 10:15:02 +0200

    Load controller thunk working with saved places in DB

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./App.css";
 import { MapContainer } from "react-leaflet";
 import MapLocation from "./components/Map/MapLocation";
+import { useAppDispatch } from "./redux/store/hooks";
+import { loadLocationsThunk } from "./redux/thunks/locationsThunk";
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadLocationsThunk());
+  }, [dispatch]);
+
   return (
     <div className="main">
       <h1>Today City Places</h1>
+
       <MapContainer
         center={[41.3879, 2.16992]}
         zoom={12}
--- src/redux/thunks/locationsThunk.tsx
+++ src/redux/thunks/locationsThunk.tsx
@@ -3,12 +3,11 @@ import { loadLocationActionCreator } from "../features/locationsSlice";
 import { AppDispatch } from "../store/store";
 
 export const loadLocationsThunk = () => async (dispatch: AppDispatch) => {
-  const url: string = `${process.env.REACT_APP_API_URL}list`;
-
+  const url: string = `${process.env.REACT_APP_API_URL}locations/list`;
   try {
-    const { data: location } = await axios.get(url);
-    if (location) {
-      dispatch(loadLocationActionCreator(location));
+    const { data } = await axios.get(url);
+    if (data) {
+      dispatch(loadLocationActionCreator(data));
     }
   } catch (error) {
     console.log("Request doesn't work");
