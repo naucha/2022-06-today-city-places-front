d2419385ad5674d8bf0bb02ceb638e4be7bd6fb0
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-07-18 15:39:32 +0200

    Create loginPage and login path

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Navigate, Routes, Route } from "react-router-dom";
 import "./index.css";
 import { HomePage } from "./pages/Home/HomePage";
+import LoginPage from "./pages/Login/LoginPage";
 import { Notfound404 } from "./pages/Notfound404/Notfound404";
 import { RegisterPage } from "./pages/Register/RegisterPage";
 import { useAppDispatch } from "./redux/store/hooks";
@@ -18,6 +19,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Navigate to={"/home"} />} />
       <Route path="/home" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/*" element={<Notfound404 />} />
     </Routes>
