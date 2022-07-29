4cf500a74859e6f58e4a9f6723210eb6a19c92db
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-07-29 09:47:47 +0200

    Add a addForm component to homePage

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import { MapContainer } from "react-leaflet";
+import FormAdd from "../../components/Form/FormAdd/FormAdd";
 import MapLocation from "../../components/Maplocation/MapLocation";
 
-export const HomePage = () => {
+export const HomePage = (): JSX.Element => {
+  const [coordinates, setCoordinates] = useState<number[]>([0, 0]);
+
+  const [formAdd, setFormAdd] = useState(false);
+
   return (
-    <div className="main">
-      <h1>Today City Places</h1>
-      <MapContainer
-        center={[41.3879, 2.16992]}
-        zoom={12}
-        scrollWheelZoom={false}
-        className="map"
-      >
-        <MapLocation />
-      </MapContainer>
-    </div>
+    <>
+      {formAdd ? <FormAdd coordinates={coordinates} /> : ""}
+      <div className="main">
+        <h1>Today City Places</h1>
+        <MapContainer
+          center={[41.3879, 2.16992]}
+          zoom={12}
+          scrollWheelZoom={false}
+          className="map"
+        >
+          <MapLocation
+            setCoordinates={setCoordinates}
+            setFormAdd={setFormAdd}
+          />
+        </MapContainer>
+      </div>
+    </>
   );
 };
