e3536d1d36e72e7d1f946b831506b661c651efe5
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-08-01 15:01:56 +0200

    Create a styled Navlink component

---
+++ src/styles/NavlinkStyled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const NavlinkStyled = styled(NavLink)`
+  font-size: 1em;
+  font-weight: 600;
+  text-decoration: none;
+  color: var(--fc-primary);
+
+  &:hover {
+    color: var(--red);
+  }
+`;
