59f7b515a4e613d7d0bb0fe92ee1e4d8b4f545ba
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-08-01 15:08:18 +0200

    Create a style of forms

---
+++ src/styles/FormStyled.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const FormStyled = styled.form`
+  width: 75vw;
+  border-radius: 6px;
+  height: calc(100vh);
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-items: center;
+  position: absolute;
+  background-color: #dededed4;
+  background-color: #122637b9;
+  top: 35vh;
+  bottom: 10vh;
+  z-index: 3;
+  gap: 10px;
+
+  input {
+    width: 40vw;
+    height: 35px;
+    font-size: 0.6em;
+    padding-left: 10px;
+    border: 2px solid var(--bg-primary);
+    border-radius: 14px;
+  }
+
+  label {
+    width: 40vw;
+    font-size: 1.2em;
+    font-weight: 600;
+    padding: 2px;
+    color: var(--fc-primary);
+  }
+
+  .actions__form {
+    width: 40vw;
+    display: flex;
+    margin: 0;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  a {
+    text-decoration: none;
+    font-size: 0.8em;
+    font-weight: 600;
+    letter-spacing: 2px;
+    color: var(--fc-primary);
+  }
+`;
