6bc44679f6269d48fc3a7072e30bc7bc6f88ae4a
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-08-01 15:09:52 +0200

    Add styled form addForm form, add savebutton too

--- src/components/Form/FormAdd/FormAdd.tsx
+++ src/components/Form/FormAdd/FormAdd.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../redux/store/hooks";
 import { addLocationThunk } from "../../../redux/thunks/locationsThunk";
 import { LocationData } from "../../../types/types";
+import { SaveButtonStyled } from "../../Button/SaveButtonStyled/SavePlaceStyled";
+import { FormStyled } from "../../../styles/FormStyled";
+import HeadingSection from "../../Headings/HeadingSection";
 
 interface coordinatesData {
   coordinates: number[];
@@ -49,43 +52,41 @@ const FormAdd = ({ coordinates }: coordinatesData): JSX.Element => {
   };
 
   return (
-    <>
-      <form noValidate autoComplete="off" onSubmit={submitData}>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          value={formData.name}
-          onChange={changeData}
-          placeholder={"Name the place"}
-        ></input>
-
-        <label htmlFor="description">Description</label>
-        <input
-          id="description"
-          value={formData.description}
-          onChange={changeData}
-          placeholder={"Describe the place"}
-        ></input>
-
-        <label htmlFor="media">Media</label>
-        <input
-          id="media"
-          value={formData.media}
-          onChange={changeData}
-          placeholder={"Media of the place"}
-        ></input>
-
-        <label htmlFor="type">Type</label>
-        <input
-          id="type"
-          value={formData.type}
-          onChange={changeData}
-          placeholder={"Type"}
-        ></input>
-
-        <button type="submit">Save Place</button>
-      </form>
-    </>
+    <FormStyled noValidate autoComplete="off" onSubmit={submitData}>
+      <HeadingSection className="heading__form" name="ADD A NEW LOCATION" />
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        value={formData.name}
+        onChange={changeData}
+        placeholder={"Name the place"}
+      ></input>
+      <label htmlFor="description">Description</label>
+      <input
+        id="description"
+        value={formData.description}
+        onChange={changeData}
+        placeholder={"Describe the place"}
+      ></input>
+      <label htmlFor="media">Media</label>
+      <input
+        id="media"
+        value={formData.media}
+        onChange={changeData}
+        placeholder={"Media of the place"}
+      ></input>
+      <label htmlFor="type">Type</label>
+      <input
+        id="type"
+        value={formData.type}
+        onChange={changeData}
+        placeholder={"Type"}
+      ></input>
+      <SaveButtonStyled type="submit">Save Place</SaveButtonStyled>
+      <div>
+        <span>X</span>
+      </div>
+    </FormStyled>
   );
 };
 
