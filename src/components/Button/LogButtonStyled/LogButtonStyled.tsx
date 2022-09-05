0e68aebc96dd6fa8229625ac1822ca841c62e144
ignasi reixach urcola <ignasi@MacBookRu.local>
2022-09-05 12:23:06 +0200

    Style log button

---
+++ src/components/Button/LogButtonStyled/LogButtonStyled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import ButtonStyled from "../ButtonStyled";
+
+export const LogButtonStyled = styled(ButtonStyled)`
+  width: 25%;
+  height: 40px;
+  border: 2px solid #ff6850;
+  background-color: transparent;
+  font-weight: 800;
+  letter-spacing: 1px;
+`;
--- src/components/Button/SaveButtonStyled/SavePlaceStyled.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-import ButtonStyled from "../ButtonStyled";
-
-export const SaveButtonStyled = styled(ButtonStyled)`
-  width: 20vw;
-  height: 40px;
-`;
